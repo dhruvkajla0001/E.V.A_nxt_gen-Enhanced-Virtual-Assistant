d1229bd2da0a7e01bb9a75b098886e68ad6363ff
MukuSick2 <mukund3818.be22@chitkara.edu.in>
2025-07-18 13:40:34 +0530

    all client

--- eva-2.0/client/my-project/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
-// This Vite configuration file sets up Tailwind CSS for the project.
-// It imports the necessary modules and exports a configuration object that includes the Tailwind CSS plugin.
